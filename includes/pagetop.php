8295a2b6cc2f0552d60af3135fc4986319b832a6
Laura Ristoff <lauraristoff83@gmail.com>
2021-10-22 16:50:30 -0400

    update includes

--- includes/pagetop.php
+++ includes/pagetop.php
@@ -1,7 +1,7 @@
 <?php
 /* $siteroot points to the development folder.
    Reset it to an empty string when deploying the live site. */
-$siteroot = '/handp_final';
+$siteroot = '';
 // date_default_timezone_set('America/Los_Angeles');
 ?>
 	<div id="header" role="banner">
